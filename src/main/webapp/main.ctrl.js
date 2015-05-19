bcc06781f58eb06f72a3f8f6f720cd67108136d1
tgranbacka <tgranbacka@gmail.com>
2015-05-19 23:12:54 +0200

    Lagt till htmlsidor plus Angular-skelett

---
+++ src/main/webapp/main.ctrl.js
@@ -0,0 +1,6 @@
+angular.module('app').controller("MainController", function(){
+    var vm = this;
+    vm.title = 'Simple mail client';
+    vm.arende = "Ange ärende";
+    vm.adress = "adress@doman.se";
+});
